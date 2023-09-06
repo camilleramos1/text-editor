d3bd4ad901339498c4f4fa0459e10145fc908178
camilleramos1 <129894673+camilleramos1@users.noreply.github.com>
2023-09-06 14:23:01 -0600

    added js for client folder

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,40 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Putting data into the database');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+
+  console.log('Successfully saved data to database!', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('Getting data from the database...');
 
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+
+  console.log('Data retrieved successfully', result);
+
+  return result;
+};
+
+// Start the db
 initdb();
